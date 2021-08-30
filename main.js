fe04bfecbf2e9634516bccdf1fe8d10314a71129
JulianaArcanjo <89355911+JulianaArcanjo@users.noreply.github.com>
2021-08-30 18:43:10 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+<html lang= "pt-br">
+<head>
+      <title> Minha página</title>
+ 
+"fontSize": 18
+"style":italic
+"color":violet
+
+      <h1> Minha página</h1>
+      <meta charset= "utf-8"></meta>
+     <script type="text/javacript" src="./js/main.js">
+     </script>
+</head>
+<body>
+      <script>
+
+      lista.push("Milu");
+            console.log(lista)
+
+function Mikebb(nome, idade, frase)
+      alert ("Bem vindo " + nome);
+            console.log(nome,idade,frase);
+
+Mikebb( "Mike Mamãe",4,"miuu");
+Mikebb( "Milu",4, "meownnnnnnnnnn");
+Mikebb("Regis", 42, "meu leite com nescau");
+Mikebb("Juliana", 41, "Whatever it takes");
+
+</script>
+<h1>Juliana Arcanjo</h1>
+
+
+</body>
+</html>
